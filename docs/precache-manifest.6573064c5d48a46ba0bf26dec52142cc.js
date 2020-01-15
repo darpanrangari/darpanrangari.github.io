e777d9bb4e6d289507f44f0b0bf957117581c271
darpan.rangari <darpan.rangari@lloydsbanking.com>
2020-01-15 19:42:21 +0000

    updated docs folder

---
+++ docs/precache-manifest.6573064c5d48a46ba0bf26dec52142cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9daee066482d66f230050365260076d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ddb0791ade2c023b361",
+    "url": "/static/js/2.4150d73b.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.4150d73b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06f0573c58c806a96979",
+    "url": "/static/js/main.1e9596d1.chunk.js"
+  },
+  {
+    "revision": "854f264a33917d128752",
+    "url": "/static/js/runtime-main.45618f02.js"
+  }
+]);
